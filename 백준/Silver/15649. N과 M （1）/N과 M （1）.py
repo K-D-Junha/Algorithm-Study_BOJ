10079336baf0642d85d68e940082642ca18594f3
K-D-Junha <48269853+K-D-Junha@users.noreply.github.com>
2022-12-13 23:46:16 +0900

    [Silver III] Title: N과 M (1), Time: 196 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,23 @@
+def promising(seq, i):
+    if i in seq:
+        return False
+    return True
+    
+def dfs(seq, N, toPick):
+    if toPick == 0:
+        print(' '.join(map(str,seq)))
+        return
+    for i in range(1, N+1):
+        if promising(seq, i) == True:
+            seq.append(i)
+            dfs(seq, N, toPick-1)
+            seq.pop(-1)
+    
+
+N, M = list(map(int, input().split()))
+
+seq = []
+for i in range(1,N+1):
+    seq.append(i)
+    dfs(seq, N, M-1)
+    seq.pop(-1)
